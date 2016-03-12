656ac3f9ccd456a98d345f3ce8c2860f93f9a074
Home <Home@armdev.ubuntu.com>
2016-03-12 21:40:35 +0400

    scala app

---
+++ scala-maven/src/main/scala/com/scala/tutorial/HelloWorld.scala
@@ -0,0 +1,12 @@
+
+package com.scala.tutorial
+
+object HelloWorld extends App {
+  println("Hello, world!")
+  var firstName: String = "Armen"
+  var lastName: String = "Arzumanyan"
+  println("My name is  " + firstName + " lastname is " + lastName)
+  var age: Int = 40
+  println("I am   " + age + " years old! ")
+
+}
---
+++ scala-maven/src/main/scala/com/scala/tutorial/Main.scala
@@ -0,0 +1,6 @@
+package com.scala.tutorial
+
+object Main extends App with Logging {
+  def msg = "Hello World"
+  log.info(msg)
+}
